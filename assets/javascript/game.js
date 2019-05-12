0000386e8af3f73f1c4132370aafcdde5b125312
Enragedtoast <edwardsjtimothy@gmail.com>
2019-05-12 17:37:10 -0500

    added basic content to html, linked css and js files

---
+++ assets/javascript/game.js
@@ -0,0 +1,25 @@
+var randNum = "";
+var playerGuess = "";
+var crystal1 = "";
+var crystal2 = "";
+var crystal3 = "";
+var crystal4 = "";
+
+//display randomn number between 19 -120
+
+
+
+//display player guess
+
+//display four buttons
+    //assign each a random number between 1-12 
+    //on button click, add number to player guess
+
+
+//display scoreboard 
+    //track wins and loses
+        //if player guess > random number, add one to losses 
+        //if player guess = random number, add one to wins
+
+//generate new numbers for buttons and random number upon win or loss and restart
+    //score should remain
