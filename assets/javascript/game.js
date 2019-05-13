4f965f22a54c23253bdde72aae0431cc4ac5cd71
Enragedtoast <edwardsjtimothy@gmail.com>
2019-05-13 12:56:36 -0500

    added code for crystal buttons to add value to guess

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-var playerGuess = $("#player-num");
+var playerGuess = 0;
 var crystalImg = ["http://cdn.playbuzz.com/cdn/35910209-2844-45c0-b099-f4d82878d54f/00261fda-4062-4096-81fd-8cf96b9034e8.jpg", "https://cdn.shopify.com/s/files/1/0008/3682/9247/products/Light_Purple_Tanzanite_Crystal_1_800x.jpg?v=1542483600", "https://d2f0ora2gkri0g.cloudfront.net/b4/31/b4310115-5a45-46a3-8b46-52785bd0a777.png", "https://cdn-images-1.medium.com/max/2600/0*AmwIyi21fDd9Guwz.jpg"];
 var test = $("#test");
 var hiddenNums = [];
@@ -8,7 +8,7 @@ var score = 0;
 
 //display randomn number between 19 -120
 
-$("#random-num").text(Math.floor((Math.random() * 102) + 19));
+var targetNum = $("#random-num").text(Math.floor((Math.random() * 102) + 19));
 
 //generate array of 4 numbers 
 
@@ -17,7 +17,7 @@ $("#random-num").text(Math.floor((Math.random() * 102) + 19));
 
         var crystals = $("<img>");
         crystals.addClass("crystal-image");
-        crystals.attr("crystal-value", hiddenNums[i]);
+        crystals.attr("data-crystalvalue", hiddenNums[i]);
         crystals.attr("src", crystalImg[i]);
         $(".crystal-btn").append(crystals); 
     
@@ -26,10 +26,10 @@ $("#random-num").text(Math.floor((Math.random() * 102) + 19));
 //on button click, assign crystal a value 
 
 $(".crystal-image").on("click", function() {
-
-
-
-}
+    var crystalValue = ($(this).attr("data-crystalvalue"));
+    crystalValue = parseInt(crystalValue);
+   $("player-num").text(playerGuess += crystalValue);
+});
 
 
 
