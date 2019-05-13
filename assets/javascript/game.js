da92b188910d5c7f58fff0245e8a8461cc49e996
Enragedtoast <edwardsjtimothy@gmail.com>
2019-05-13 12:42:30 -0500

    added images to crystal buttons

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,7 @@
 $(document).ready(function() {
 
 var playerGuess = $("#player-num");
-var crystal1 = $("#button-one");
-var crystal2 = $("#button-two");
-var crystal3 = $("#button-three");
-var crystal4 = $("#button-four");
+var crystalImg = ["http://cdn.playbuzz.com/cdn/35910209-2844-45c0-b099-f4d82878d54f/00261fda-4062-4096-81fd-8cf96b9034e8.jpg", "https://cdn.shopify.com/s/files/1/0008/3682/9247/products/Light_Purple_Tanzanite_Crystal_1_800x.jpg?v=1542483600", "https://d2f0ora2gkri0g.cloudfront.net/b4/31/b4310115-5a45-46a3-8b46-52785bd0a777.png", "https://cdn-images-1.medium.com/max/2600/0*AmwIyi21fDd9Guwz.jpg"];
 var test = $("#test");
 var hiddenNums = [];
 var score = 0;
@@ -13,25 +10,26 @@ var score = 0;
 
 $("#random-num").text(Math.floor((Math.random() * 102) + 19));
 
-//generate array of 4 numbers, generate four images
+//generate array of 4 numbers 
 
     for (i = 0; i < 4; i++) {
         hiddenNums.push(Math.floor(Math.random() * 12) + 1);
 
         var crystals = $("<img>");
         crystals.addClass("crystal-image");
-        crystals.attr("src", "http://cdn.playbuzz.com/cdn/35910209-2844-45c0-b099-f4d82878d54f/00261fda-4062-4096-81fd-8cf96b9034e8.jpg")
         crystals.attr("crystal-value", hiddenNums[i]);
+        crystals.attr("src", crystalImg[i]);
         $(".crystal-btn").append(crystals); 
-        
-        $("#button-one").text("hello")
+    
 }
 
-//on button click, add number to player guess
+//on button click, assign crystal a value 
 
-// crystal1.on("click", function () {
-//     playerGuess.text(hiddenNums[0]);
-// })
+$(".crystal-image").on("click", function() {
+
+
+
+}
 
 
 
