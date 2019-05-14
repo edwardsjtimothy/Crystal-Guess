0355a0e2741ae130ed809958a98ce988b519804f
Enragedtoast <edwardsjtimothy@gmail.com>
2019-05-13 23:16:09 -0500

    trying to fix win and loss counters

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,22 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-var playerGuess = 0;
-var crystalImg = ["http://cdn.playbuzz.com/cdn/35910209-2844-45c0-b099-f4d82878d54f/00261fda-4062-4096-81fd-8cf96b9034e8.jpg", "https://cdn.shopify.com/s/files/1/0008/3682/9247/products/Light_Purple_Tanzanite_Crystal_1_800x.jpg?v=1542483600", "https://d2f0ora2gkri0g.cloudfront.net/b4/31/b4310115-5a45-46a3-8b46-52785bd0a777.png", "https://cdn-images-1.medium.com/max/2600/0*AmwIyi21fDd9Guwz.jpg"];
-var test = $("#test");
-var hiddenNums = [];
-var score = 0;
+    var playerGuess = 0;
+    var crystalImg = ["http://cdn.playbuzz.com/cdn/35910209-2844-45c0-b099-f4d82878d54f/00261fda-4062-4096-81fd-8cf96b9034e8.jpg", "https://cdn.shopify.com/s/files/1/0008/3682/9247/products/Light_Purple_Tanzanite_Crystal_1_800x.jpg?v=1542483600", "https://d2f0ora2gkri0g.cloudfront.net/b4/31/b4310115-5a45-46a3-8b46-52785bd0a777.png", "https://cdn-images-1.medium.com/max/2600/0*AmwIyi21fDd9Guwz.jpg"];
+    var hiddenNums = [];
+    var playerWins = 0;
+    var playerLosses = 0;
 
-//display randomn number between 19 -120
 
-var targetNum = $("#random-num").text(Math.floor((Math.random() * 102) + 19));
+    //display scoreboard 
+    $("#wins").text(playerWins);
+    $("#losses").text(playerLosses);
+    $("#player-num").text(playerGuess);
 
-//generate array of 4 numbers 
+    //display randomn number between 19 -120
+
+    var targetNum = $("#random-num").text(Math.floor((Math.random() * 102) + 19));
+
+    //generate array of 4 numbers 
 
     for (i = 0; i < 4; i++) {
         hiddenNums.push(Math.floor(Math.random() * 12) + 1);
@@ -19,31 +25,33 @@ var targetNum = $("#random-num").text(Math.floor((Math.random() * 102) + 19));
         crystals.addClass("crystal-image");
         crystals.attr("data-crystalvalue", hiddenNums[i]);
         crystals.attr("src", crystalImg[i]);
-        $(".crystal-btn").append(crystals); 
-    
-}
+        $(".crystal-btn").append(crystals);
+    };
 
-//on button click, assign crystal a value 
+    //on button click, assign crystal a value 
 
-$(".crystal-image").on("click", function() {
-    var crystalValue = ($(this).attr("data-crystalvalue"));
-    crystalValue = parseInt(crystalValue);
-   $("#player-num").text(playerGuess += crystalValue);
-});
+    $(".crystal-image").on("click", function () {
+        var crystalValue = ($(this).attr("data-crystalvalue"));
+        crystalValue = parseInt(crystalValue);
+        $("#player-num").text(playerGuess += crystalValue);
+        playerGuess += crystalValue;
 
-
-
-
-
-
-
-//display scoreboard 
-    //track wins and loses
-        //if player guess > random number, add one to losses 
         //if player guess = random number, add one to wins
 
-//generate new numbers for buttons and random number upon win or loss and restart
-    //score should remain
+        if (playerGuess === targetNum) {
+            ++playerWins;
+            $
+        }
+
+        // //if player guess > random number, add one to losses 
+
+        else if (playerGuess >= targetNum) {
+            ++playerLosses;
+
+
+        }
+
+    });
 
 
 
